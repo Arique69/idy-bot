848741169466458c123551341a45ed5952267f53
Arique69 <mhmdariq88@gmail.com>
2026-05-09 01:04:23 +0700

    Add My Bini common card

--- config.py
+++ config.py
@@ -79,6 +79,11 @@ CARDS: list[dict] = [
         "name": "Rusdi Kapan yh",
         "rarity": "common",
     },
+    {
+        "url": "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bHg0djFjcHhhYzZnYzZydDkzNDhqdDZ1eXJxY2ZlemNqZHUxOGo3ayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/FHUq34j8vRBVS01uSj/giphy.gif",
+        "name": "My Bini",
+        "rarity": "common",
+    },
     # --- Rare 🔵 ---
     {
         "url": "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGdwc3NlOWRndG90bm54c2VhanFscGJheWV5dG9nZzI2NGNyd25lYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H7oe4YDsrMbNuzCF1g/giphy.gif",
